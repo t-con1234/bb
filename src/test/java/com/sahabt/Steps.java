2557ded5e2d8bbfe0cbc31639efe678dca227b3d
Selçuk Egeli <selcuk.egeli@sahabt.com>
2018-01-31 16:00:35 +0300

    Initial commit

---
+++ src/test/java/com/sahabt/Steps.java
@@ -0,0 +1,52 @@
+package com.sahabt;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Then;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import cucumber.api.java.en.Given;
+
+
+public class Steps{
+
+    public String URL = "http://hub.testinium.io/wd/hub";
+    public static RemoteWebDriver driver;
+
+    @Before
+    public void before() {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        /*capabilities.setCapability("key", System.getProperty("key"));
+
+        try {
+            driver = new RemoteWebDriver(new URL(URL),capabilities);*/
+            driver = new ChromeDriver();
+        /*} catch (MalformedURLException e) {
+            e.printStackTrace();
+        }*/
+    }
+
+    @After
+    public void afterMethod(){
+       driver.quit();
+    }
+
+    @Given("^Go to \"(.*)\"")
+    public void goTo(String url) {
+        driver.navigate().to(url);
+    }
+
+    @Then("^Wait \"([^\"]*)\" ms$")
+    public void waitMs(String time) throws Throwable {
+        try {
+            Thread.sleep(Integer.valueOf(time));
+        } catch (InterruptedException e) {
+        }
+    }
+}
---
+++ src/test/resources/features/example.feature
@@ -0,0 +1,6 @@
+Feature: Online Process 
+
+@goToWebsite
+Scenario: Website Test Scenario
+	Given Go to "https://www.amazon.com"
+	Then Wait "3000" ms
